af2daaf7258f8b62666d97e13d30dc7c0d4a8b6e
ishween <ishweensehmbhi@gmail.com>
2022-04-20 22:04:18 -0400

    add core nice quote functionality

--- bot.js
+++ bot.js
@@ -2,6 +2,21 @@ require("dotenv").config();
 
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const fetch = require("node-fetch");
+
+function getAffirmation() {
+	return fetch("https://www.affirmations.dev/")
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			return data.affirmation;
+		});
+}
+
+function getGif() {
+	return fetch();
+}
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -10,12 +25,14 @@ client.on("ready", () => {
 client.on("interactionCreate", async (interaction) => {
 	if (!interaction.isCommand()) return;
 
-	if (interaction.commandName === "plshug") {
+	if (interaction.commandName === "bearhug") {
 		await interaction.reply("bring it in for a big bear hug!");
+	} else if (interaction.commandName === "bearnice") {
+		await getAffirmation().then((quote) => {
+			interaction.reply(quote);
+		});
 	} else if (interaction.commandName === "owo") {
 		await interaction.reply("uwu");
-	} else if (interaction.commandName === "meow") {
-		await interaction.reply("rawr");
 	}
 });
 
--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-// Run this to register any slash commands
-// This doesn't work yet, one time run only
-
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+const token = process.env.BOT_TOKEN;
 
 const commands = [
 	{
-		name: "ping",
-		description: "Replies with Pong!",
+		name: "bearhug",
+		description: "bearbot will give you a big bear hug!",
+	},
+	{
+		name: "bearnice",
+		description:
+			"bearbot is here for you and will tell you something kind",
+	},
+	{
+		name: "owo",
+		description: "bearbot will uwu at you",
 	},
 ];
 
-const rest = new REST({ version: "9" }).setToken(process.env.BOT_TOKEN);
+const rest = new REST({ version: "9" }).setToken(token);
 
 (async () => {
 	try {
